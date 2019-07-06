6a2ec02c1573bd2514b0fee876524e1a4d3b2654
mrvettleson <mrvettleson@gmail.com>
2019-07-05 21:55:32 -0500

    Added Sample code for Temp/Hum Sensor

--- HumidityTempSensorSampleOnDisplay/HumidityTempSensorSampleOnDisplay.ino
+++ HumidityTempSensorSampleOnDisplay/HumidityTempSensorSampleOnDisplay.ino
@@ -1,28 +1,29 @@
-
+#include <dht.h>
 #include "U8glib.h"
-int dat = 0;
+int tem = 0;
+int hum = 0;
 
 U8GLIB_SH1106_128X64 u8g(13, 11, 10, 9);  //(clock, Mosi,cs, ao or DC on mine)
+dht DHT;
 
+#define DHT11_PIN 4
 void draw() {
   
   // graphic commands to redraw the complete screen should be placed here  
   u8g.setFont(u8g_font_unifont);
-  //u8g.setFont(u8g_font_osb21);
+  
   u8g.setPrintPos(0,22);
   u8g.print("Temp: ");
   u8g.setPrintPos(50,22);
-  u8g.print(dat);
-  //u8g.drawStr( 0, 22, "Hello Kay!"); //This is my output here
+  u8g.print(tem);
+  u8g.setPrintPos(0,50);
+  u8g.print("Humidity: ");
+  u8g.setPrintPos(75,50);
+  u8g.print(hum);
 }
-void setup(void) {
-  Serial.begin(9600);//Set Baud Rate to 9600 bps
-  // flip screen, if required
-  // u8g.setRot180();
-   // set SPI backup if required
-  //u8g.setHardwareBackup(u8g_backup_avr_spi);
- // assign default color value
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+void setup(){
+  Serial.begin(9600);
+ if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
     u8g.setColorIndex(255);     // white
   }
   else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
@@ -33,25 +34,27 @@ void setup(void) {
   }
   else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
     u8g.setHiColorByRGB(255,255,255);
-  }}
-void loop(void) {
-  // Temp Loop
-  int val;
-    
-    val=analogRead(0);//Connect LM35 on Analog 0
-    dat=(500 * val) /1024;;
-    Serial.print("Temp:"); //Display the temperature on Serial monitor
-    Serial.print(dat);
-    Serial.println("C");
-    ;
-    delay(500);
+  }
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  tem = DHT.temperature*9/5 + 32;//Set as C, I converted it to F
+  hum = DHT.humidity;
   
   // picture loop
   u8g.firstPage();  
   do {
     draw();
-  } while( u8g.nextPage() );
+      } while( u8g.nextPage() );
    
    // rebuild the picture after some delay
-  delay(50);
+  delay(1000);
 }
+  //delay(1000);
+//}
